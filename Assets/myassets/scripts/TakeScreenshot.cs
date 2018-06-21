d9894543a3f028a26f9055c08898f117726bb49c
Revortni <rojit.blaze13@gmail.com>
2018-06-21 20:49:37 +0545

    added dialog to indicate screen capture

--- Assets/myassets/scripts/TakeScreenshot.cs
+++ Assets/myassets/scripts/TakeScreenshot.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TakeScreenshot : MonoBehaviour {
 
 	[SerializeField]
+	public GameObject info;
 
+	void Start(){
+		info.SetActive(false);
+	}
 	public void TakeAShot()
 	{
 		StartCoroutine ("CaptureIt");
@@ -13,11 +18,14 @@ public class TakeScreenshot : MonoBehaviour {
 
 	IEnumerator CaptureIt()
 	{
+
 		string timeStamp = System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss");
 		string fileName = "Screenshot" + timeStamp + ".png";
 		string pathToSave = fileName;
 		ScreenCapture.CaptureScreenshot(pathToSave);
-		yield return new WaitForEndOfFrame();
+		info.SetActive(true);
+		yield return new WaitForSeconds(2);
+		info.SetActive(false);
 	}
 
 }
