83223ea0fe6579073232dc3130c5f0cbfa238239
Revortni <rojit.blaze13@gmail.com>
2018-08-05 18:49:24 +0545

    scene change fixes for logged and not logged in

--- Assets/myassets/scripts/changeScenes.cs
+++ Assets/myassets/scripts/changeScenes.cs
@@ -6,13 +6,36 @@ using UnityEngine.SceneManagement;
 
 public class changeScenes : MonoBehaviour {
 	private int scene;
+
+	public void Start(){
+		if(SceneManager.GetActiveScene().name == "home"){
+			PlayerPrefs.SetInt("loggedin")=0;
+		}
+	}
+
 	public void changeScene(int sceneNumber)
 	{	
 		scene = sceneNumber;
-		if(SceneManager.GetActiveScene().name == "home" && sceneNumber==0)
-			Application.Quit();
-		else
-			SceneManager.LoadScene(sceneNumber);
+
+		if(PlayerPrefs.GetInt("loggedin")==1){
+			if(SceneManager.GetActiveScene().buildIndex==4){
+				if(scene==0){
+					scene=1;
+				} else {
+					SceneManager.LoadScene(scene);
+				}
+			}
+			else{
+				SceneManager.LoadScene(scene);
+			}
+		} else {
+				if(SceneManager.GetActiveScene().name == "home" && sceneNumber==0)
+					Application.Quit();
+				else
+					{
+						SceneManager.LoadScene(scene);
+					}
+		}
 	}
 
 	void Update(){
